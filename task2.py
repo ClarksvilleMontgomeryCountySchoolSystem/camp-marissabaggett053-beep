589b7f51ada4b7ae14f8c9966c82cc0b80dd13cc
marissabaggett053-beep <marissa.baggett053@students.cmcss.net>
2026-02-11 11:52:37 -0600

    y

--- task2.py
+++ task2.py
@@ -7,34 +7,36 @@ from data import first, last, birth, days, p_first, p_last, phone, street, city,
 
 
 # first
-
+print(first)
 
 # last
-
+print(last)
 
 # birth
-
+print(birth)
+birth = int(birth)
 
 # days
-
+print(days)
+days = int(days)
 
 # p_first
-
+print(p_first)
 
 # p_last
-
+print(p_last)
 
 # phone
-
+print(phone)
 
 # street
-
+print(street)
 
 # city
-
+print(city)
 
 # state
-
+print(state)
 
 # zip_code
-
+print(zip_code)
